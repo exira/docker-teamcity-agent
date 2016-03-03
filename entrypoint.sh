cc6c646b4cd78f8757b28818efa318f02b1ae932
sashgorokhov <sashgorokhov@gmail.com>
2016-03-03 10:59:14 +0300

    Updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ done
 wget ${SERVER_URL}/update/buildAgent.zip && unzip -d ${AGENT_DIR} buildAgent.zip && rm buildAgent.zip
 
 echo "serverUrl=${SERVER_URL}" > ${AGENT_DIR}/conf/buildAgent.properties
-echo "name=${AGENT_NAME}" >> ${AGENT_DIR}/conf/buildAgent.properties
+# echo "name=${AGENT_NAME}" >> ${AGENT_DIR}/conf/buildAgent.properties
 echo "workDir=${AGENT_WORKDIR}" >> ${AGENT_DIR}/conf/buildAgent.properties
 echo "tempDir=${AGENT_TEMPDIR}" >> ${AGENT_DIR}/conf/buildAgent.properties
 echo "ownPort=${AGENT_OWN_PORT}" >> ${AGENT_DIR}/conf/buildAgent.properties
