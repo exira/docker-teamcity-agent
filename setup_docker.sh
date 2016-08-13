c32c32cf90a9b4689b1db88e5183068d6ba978b4
David Cumps <david@cumps.be>
2016-08-13 21:57:45 +0200

    Use docker 1.11.2

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-wget -P /tmp https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz
-tar -xzf /tmp/docker-latest.tgz -C / && rm /tmp/docker-latest.tgz
+#wget -P /tmp https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz
+wget -P /tmp https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz
+tar -xzf /tmp/docker-1.11.2.tgz -C / && rm /tmp/docker-1.11.2.tgz
+ln -s /docker/docker /usr/local/bin/docker
 echo Successfuly installed docker client
