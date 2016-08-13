f3f82ae27519e75788dae7461edabf1045ccf259
David Cumps <david@cumps.be>
2016-08-13 10:56:10 +0200

    Update node

--- setup_npm.sh
+++ setup_npm.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-apt-get install -y nodejs npm
-ln -s /usr/bin/nodejs /usr/bin/node
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | NVM_DIR=/usr/local/nvm bash
+source ~/.profile
+nvm install node
+
+#apt-get install -y nodejs npm
+#ln -s /usr/bin/nodejs /usr/bin/node
 echo Successfully installed Node and NPM
 
 npm install bower -g
