bca484ff4ad68727c19c6b1f6b6ff76933578f08
David Cumps <david@cumps.be>
2016-08-13 21:09:40 +0200

    Update setup_npm.sh

--- setup_npm.sh
+++ setup_npm.sh
@@ -3,8 +3,6 @@ set -e
 
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y nodejs
-#apt-get install -y nodejs npm
-#ln -s /usr/bin/nodejs /usr/bin/node
 echo Successfully installed Node and NPM
 
 npm install bower -g
