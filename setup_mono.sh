e6701c0ec35e58a81a8d181a19f4997e7344916d
David Cumps <david@cumps.be>
2016-08-14 02:08:42 +0200

    convulted way of installing mono

--- setup_mono.sh
+++ setup_mono.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
+apt-get install -y mono-complete
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | tee /etc/apt/sources.list.d/mono-xamarin.list
-apt-get update && apt-get install -y mono-devel mono-complete ca-certificates-mono
+apt-get update && apt-get install -y mono-devel mono-complete ca-certificates-mono mono-4.0-service libmono-cil-dev
 echo Successfully installed Mono
